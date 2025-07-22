2efeb0af9280c5c0b3a1fc04e48c730d1834d758
zhuangzhuang <17610835880@163.com>
2025-07-22 19:13:36 +0800

    修改

--- backup/冰红茶.py
+++ backup/冰红茶.py
@@ -16,10 +16,10 @@ def generate_url(base_url="HTTPS://25.KSF.CN"):
     """生成完整URL"""
     random_code = generate_random_code()
     return f"{base_url}?{random_code}"
-    # return f"{base_url}?0HC44IDXR4QQKC"
-    # HTTPS://25.KSF.CN/RN9VYHRO2U0KBG
-    # HTTPS://25.KSF.CN?0HC44IDXR4QQKC
-    # HTTPS://YYC.1U1M.CN/08H4YH9D3O4TLKV38C31
+    # return f"{base_url}?RN9VYHRO2U0KBG"
+    # HTTPS://25.KSF.CN?RN9VYHRO2U0KBG"
+    # HTTPS://25.KSF.CN?RVMFGULGYAHFAM
+    # HTTPS://25.KSF.CN?JBTJULYOQ04PDP
 
 
 def generate_qrcode(url, save_path):
@@ -40,7 +40,7 @@ def main():
     os.makedirs("qrcodes", exist_ok=True)
     
     print("生成的URL及二维码：")
-    for i in range(1, 10):  # 生成10个
+    for i in range(1, 20):  # 生成10个
         url = generate_url()
         print(f"{i}. {url}")
         
